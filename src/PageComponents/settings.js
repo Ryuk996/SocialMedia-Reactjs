901cf755ed2c395774503fabdd604a4302c3a3e3
Ryuk996 <thirunavu96@gmail.com>
2021-12-14 11:00:22 +0530

    Added react loggedstatus

--- src/PageComponents/settings.js
+++ src/PageComponents/settings.js
@@ -1,6 +1,6 @@
 let getSettings = () =>{
     return {
-        api:"http://localhost:3007"                                    //"https://loginp.herokuapp.com"5 https://sociagramz.herokuapp.com//http://localhost:3004//*https://sociagramz.herokuapp.com
+        api:"https://sociagramz.herokuapp.com"                                    //"https://loginp.herokuapp.com"5 https://sociagramz.herokuapp.com//http://localhost:3004//*https://sociagramz.herokuapp.com
     }
 }
 
